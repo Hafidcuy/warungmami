05a12b3c3eb22f07addf9684e8045a94091854e1
Hafidcuy <tulrama67@gmail.com>
2026-01-09 09:43:26 +0700

    Add files via upload

---
+++ login.js
@@ -0,0 +1,16 @@
+function login() {
+    const username = document.getElementById("username").value;
+    const password = document.getElementById("password").value;
+    const errorMsg = document.getElementById("errorMsg");
+
+    // LOGIN SEDERHANA (BISA KAMU GANTI)
+    if (username === "admin" && password === "12345") {
+      localStorage.setItem("isLogin", "true");
+      localStorage.setItem("username", username);
+
+      alert("Login berhasil!");
+      window.location.href = "index.html"; // arahkan ke warung
+    } else {
+      errorMsg.style.display = "block";
+    }
+  }
